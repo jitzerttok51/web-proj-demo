ed511226be175ff960667d78302fc2bdb6470d4a
Prodanov, Nikolay <Nikolay.Prodanov@softwareag.com>
2024-07-04 12:54:22 +0300

    Added database

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,9 +12,60 @@ provider "digitalocean" {
 }
 
 # import {
-#     id = "ec688386-dd4f-45aa-9cf7-30d35d08842b"
+#     id = "4dfe124d-0a59-4815-9d48-8703cae4ca7e"
+#     to = digitalocean_database_cluster.database-example
+# }
+
+# import {
+#     id = "4dfe124d-0a59-4815-9d48-8703cae4ca7e"
 #     to = digitalocean_app.app
 # }
+
+# terraform plan -generate-config-out=out.tf
+
+resource "digitalocean_database_cluster" "database-example" {
+  name                 = "web-app-14194"
+
+  node_count           = 1
+  region               = "fra1"
+
+  engine               = "mysql"
+  version              = "8"
+  
+  size                 = "db-s-1vcpu-1gb"
+  storage_size_mib     = "10240"
+}
+
+resource "digitalocean_database_user" "softuni-user" {
+  cluster_id = digitalocean_database_cluster.database-example.id
+  name = "softuni"
+}
+
+resource "digitalocean_database_db" "softuni-db" {
+  cluster_id = digitalocean_database_cluster.database-example.id
+  name = "softuni-db"
+}
+
+output "db_username" {
+  value = digitalocean_database_user.softuni-user.name
+}
+
+output "db_password" {
+  sensitive = true
+  value = digitalocean_database_user.softuni-user.password
+}
+
+output "db" {
+  value = digitalocean_database_db.softuni-db.name
+}
+
+locals {
+  db_url = "jdbc:mysql://${digitalocean_database_cluster.database-example.host}:${digitalocean_database_cluster.database-example.port}/${digitalocean_database_db.softuni-db.name}"
+}
+
+output "connection_string" {
+  value = local.db_url
+}
 
 resource "digitalocean_app" "demo-app" {
   spec {
@@ -27,6 +78,27 @@ resource "digitalocean_app" "demo-app" {
       instance_size_slug = "apps-s-1vcpu-0.5gb"
       name               = "demo-app-comp-test123"
 
+      env {
+        key = "DB_URL"
+        value = local.db_url
+        scope = "RUN_TIME"
+        type = "GENERAL"
+      }
+
+      env {
+        key = "DB_USER"
+        value = digitalocean_database_user.softuni-user.name
+        scope = "RUN_TIME"
+        type = "GENERAL"
+      }
+
+      env {
+        key = "DB_PASS"
+        value = digitalocean_database_user.softuni-user.password
+        scope = "RUN_TIME"
+        type = "SECRET"
+      }
+
       image {
         registry             = "jitzerttok51"
         registry_credentials = null # sensitive
@@ -37,4 +109,3 @@ resource "digitalocean_app" "demo-app" {
     }
   }
 }
-
